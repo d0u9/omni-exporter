05967581c042cd95eed22b9a5888b5146d7bfacd
Douglas Su <doug.su.au@gmail.com>
2025-03-21 16:24:55 +1100

    Fix error message

--- omni-exporter-unsafe/src/error.rs
+++ omni-exporter-unsafe/src/error.rs
@@ -8,7 +8,7 @@ pub enum Err {
 impl fmt::Display for Err {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Err::FFI(ref err) => write!(f, "C Error: {}", err),
+            Err::FFI(ref err) => write!(f, "FFI Error: {}", err),
         }
     }
 }
@@ -16,24 +16,3 @@ impl fmt::Display for Err {
 impl std::error::Error for Err {}
 
 pub type Result<T> = std::result::Result<T, Err>;
-
-impl From<std::num::ParseIntError> for Err {
-    fn from(err: std::num::ParseIntError) -> Self {
-        Err::FFI(err.to_string())
-    }
-}
-
-impl From<std::io::Error> for Err {
-    fn from(err: std::io::Error) -> Self {
-        match err.kind() {
-            std::io::ErrorKind::NotFound => Err::FFI(err.to_string()),
-            _ => Err::FFI(err.to_string()),
-        }
-    }
-}
-
-impl From<std::num::ParseFloatError> for Err {
-    fn from(err: std::num::ParseFloatError) -> Self {
-        Err::FFI(err.to_string())
-    }
-}
